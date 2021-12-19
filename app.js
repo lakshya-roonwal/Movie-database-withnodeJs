b285e6ce14bf73a1480e521375d13c977c2837d3
Lakshya <erenpool7214@gmail.com>
2021-12-19 23:26:25 +0530

    Adding script for heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app=express()
 const fetch=require('node-fetch')
 const bodyParser=require('body-parser')
-const port=80;
+const port=process.env.PORT || 8000;
 
 
 app.use(express.static('./public'));
